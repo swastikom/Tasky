04790ebf48fd78a592690d6f5760040a984011ca
Swastik Dhar <swastikdhar18@gmail.com>
2023-08-22 22:04:44 +0530

    Co-authored-by: Suchandra Dhar <Suchandram@users.noreply.github.com>

--- task.java
+++ task.java
@@ -3,25 +3,52 @@ import java.io.FileWriter;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 class Task {
-    public int id;
-    public String title;
-    public String priority;
+    private String title;
+    private int priority;
 
-    public Task(String title, String priority) {
-        this.id = nextId++;
+    public Task(String title, int priority) {
         this.title = title;
         this.priority = priority;
+    }
+
+    public String getTitle() {
+
+        return title;
+    }
+
+    public void setTitle() {
+
+        this.title = title;
+    }
+
+    public int getPriority() {
+
+        return priority;
+    }
+
+    public void setPriority() {
+
+        this.priority = priority;
+    }
+
+    @Override
+    public String toString() {
+        return "Title: " + title +
+                "\nPriority: " + priority;
+
     }
 }
 
 class Main {
+    private static ArrayList<Task> tasks = new ArrayList<>();
     private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         while (true) {
-            System.out.println("Welcome to Taanky!");
+            System.out.println("Welcome to Tasky!");
             System.out.println("1. Task List");
             System.out.println("2. Add Task");
             System.out.println("3. Delete Task");
@@ -37,8 +64,63 @@ class Main {
                 case 2:
                     addTask();
                     break;
-
+                case 3:
+                    deleteTask();
+                    break;
+                case 4:
+                    System.out.println("Exiting...");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid choice, try again!");
             }
+
         }
     }
+
+    private static void taskList() {
+        class TaskManager {
+            private List<Task> tasks;
+        
+            public TaskManager() {
+                tasks = new ArrayList<>();
+            }
+    }
+
+    private static void addTask() {
+        System.out.println("Enter Task Title: ");
+        String title = scanner.nextLine();
+        System.out.println("Enter Task Priority: ");
+        int priority = scanner.nextInt();
+        Task task = new Task(title, priority);
+        tasks.add(task);
+        System.out.println("Task Created Successfully!");
+    }
+
+    private static void deleteTask() {
+        if (tasks.isEmpty()) {
+            System.out.println("No Tasks are there!");
+            return;
+        }
+        System.out.println("Enter Task number Delete!");
+        int taskNumber = scanner.nextInt();
+        scanner.nextInt();
+    }
 }
+
+// Title:Task Name,
+// Priority:
+// High
+
+// Title:Task Name,
+// Priority:
+// High
+
+// Title:Task Name,
+// Priority:
+// High
+
+// Title:
+// Task Name,
+// Priority:
+// High

a1484d3bc0003cec341b7c4b3db4dda70596c8d0
Swastik Dhar <swastikdhar18@gmail.com>
2023-08-22 19:56:28 +0530

    structure of task created

--- task.java
+++ task.java
@@ -1,5 +1,10 @@
-class Main{
-    public static void main(String[] args) {
-        System.out.println("Suchandra loves Swastik!");
-    }
+
+
+class Task{
+    public int id;
+    public String title;
+    public String priority;
 }
+
+
+
